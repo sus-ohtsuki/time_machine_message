d143e853f0b6fdedd1bf58dca073de58e2d366f0
sus.ohtsuki <s-ohtsuki@s-ucs.com>
2023-03-26 19:07:34 +0900

    add intl

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sizer/sizer.dart';
+import 'package:time_machine_message/generated/l10n.dart';
 import 'package:time_machine_message/screen/router/router.dart';
 import 'package:time_machine_message/screen/theme/app_theme.dart';
 
@@ -13,6 +15,13 @@ class App extends StatelessWidget {
         return MaterialApp.router(
           theme: AppTheme.themeData(context),
           routerConfig: router,
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
         );
       },
     );
